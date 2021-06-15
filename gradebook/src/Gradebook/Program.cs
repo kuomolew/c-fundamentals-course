538efb44aac6849fc41465dfd3380cbebf9c4825
Vladimir Mironov <v.mironov@viseven.com>
2021-06-15 22:59:45 +0300

    lists

--- gradebook/src/Gradebook/Program.cs
+++ gradebook/src/Gradebook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Gradebook
 {
@@ -11,19 +12,31 @@ namespace Gradebook
             // double result = x + y;
             // System.Console.WriteLine(result);
 
-            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+            // var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
             // numbers[0] = 12.7;
             // numbers[1] = 12.7;
             // numbers[2] = 12.7;
-
             // var sum = numbers[0] + numbers[1] + numbers[2] + numbers[3];
-            var sum = 0.0;
+            // var sum = 0.0;
+            // foreach (double number in numbers)
+            // {
+            //     sum += number;
+            // }
+            // System.Console.WriteLine(sum);
 
-            foreach (double number in numbers)
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+            var result = 0.0;
+
+            foreach (double number in grades)
             {
-                sum += number;
+                result += number;
             }
-            System.Console.WriteLine(sum);
+
+            result /= grades.Count;
+            System.Console.WriteLine($"The average grade is {result:N3}");
+
+
 
             if (args.Length > 0)
             {
