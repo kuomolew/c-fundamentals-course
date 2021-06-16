a5f868ace256b1d368eb1b89c29d8cee34146ef3
Vladimir Mironov <kuomolew@gmail.com>
2021-06-16 15:27:10 +0300

    incapsulation

--- gradebook/src/Gradebook/Book.cs
+++ gradebook/src/Gradebook/Book.cs
@@ -5,11 +5,34 @@ namespace Gradebook
 {
     class Book
     {
+        public Book()
+        {
+            grades = new List<double>();
+        }
         public void AddGrade(double grade)
         {
 
+            grades.Add(grade);
+
+        }
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (double number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+
+            result /= grades.Count;
+            System.Console.WriteLine($"The average grade is {result:N1} \nThe highest is {highGrade} \nThe lowest is {lowGrade}");
 
 
         }
+
+        private List<double> grades;
     }
 }
--- gradebook/src/Gradebook/Program.cs
+++ gradebook/src/Gradebook/Program.cs
@@ -7,20 +7,11 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
-            var Book = new Book();
-            Book.AddGrade(89.1);
-
-            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-            grades.Add(56.1);
-            var result = 0.0;
-
-            foreach (double number in grades)
-            {
-                result += number;
-            }
-
-            result /= grades.Count;
-            System.Console.WriteLine($"The average grade is {result:N3}");
+            var book = new Book();
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.ShowStatistics();
 
         }
     }
