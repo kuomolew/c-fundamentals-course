92e64a2ff2825b67f6d5f6e04fc79ea0185d2d4b
Vladimir Mironov <kuomolew@gmail.com>
2021-06-17 20:25:09 +0300

    reference types

--- gradebook/test/Gradebook.Tests/TypeTest.cs
+++ gradebook/test/Gradebook.Tests/TypeTest.cs
@@ -6,6 +6,24 @@ namespace Gradebook.Tests
     public class TypeTests
     {
         [Fact]
+        public void CanSetNameFromReferenceOrValue()
+        {
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+            CanBookSetName(ref book1, "Another Name");
+
+            Assert.Equal("Another Name", book1.Name);
+        }
+        private void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
+        public void CanBookSetName(ref Book book, string name)
+        {
+            book = new Book(name);
+
+        }
+        [Fact]
         public void GetbookReturnsDifferentObjects()
         {
             // arrange
