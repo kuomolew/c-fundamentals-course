f5ce68bf4e27f30a8ec3830b9e80013d0c2576ca
Vladimir Mironov <kuomolew@gmail.com>
2021-06-18 17:13:32 +0300

    switch

--- gradebook/src/Gradebook/Book.cs
+++ gradebook/src/Gradebook/Book.cs
@@ -10,6 +10,34 @@ namespace Gradebook
             grades = new List<double>();
             Name = name;
         }
+
+        public void AddLetterGrade(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                case 'D':
+                    AddGrade(60);
+                    break;
+                case 'E':
+                    AddGrade(50);
+                    break;
+                case 'F':
+                    AddGrade(40);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
         public void AddGrade(double grade)
         {
             if (grade <= 100 && grade >= 0)
@@ -34,12 +62,6 @@ namespace Gradebook
             // for loop
             for (var index = 0; index < grades.Count; index++)
             {
-                if (grades[index] == 42.1)
-                {
-                    //break;
-                    continue;
-                }
-
                 result.High = Math.Max(grades[index], result.High);
                 result.Low = Math.Min(grades[index], result.Low);
                 result.Average += grades[index];
