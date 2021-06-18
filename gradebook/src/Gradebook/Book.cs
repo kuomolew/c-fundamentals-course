a50a13da51b0241c60f51aafd6f3b80f4cdc2c2e
Vladimir Mironov <kuomolew@gmail.com>
2021-06-18 17:05:33 +0300

    jumping statements

--- gradebook/src/Gradebook/Book.cs
+++ gradebook/src/Gradebook/Book.cs
@@ -31,39 +31,15 @@ namespace Gradebook
             result.High = double.MinValue;
             result.Low = double.MaxValue;
 
-            //foreach loop
-            // foreach (double grade in grades)
-            // {
-            //     result.High = Math.Max(grade, result.High);
-            //     result.Low = Math.Min(grade, result.Low);
-            //     result.Average += grade;
-            // }
-
-            // do while loop
-            // int index = 0;
-            // do
-            // {
-            //     result.High = Math.Max(grades[index], result.High);
-            //     result.Low = Math.Min(grades[index], result.Low);
-            //     result.Average += grades[index];
-            //     index++;
-            // }
-            // while (index < grades.Count);
-
-            // while loop
-            // int index = 0;
-            // while (index < grades.Count)
-            // {
-            //     result.High = Math.Max(grades[index], result.High);
-            //     result.Low = Math.Min(grades[index], result.Low);
-            //     result.Average += grades[index];
-            //     index++;
-            // }
-
-
             // for loop
             for (var index = 0; index < grades.Count; index++)
             {
+                if (grades[index] == 42.1)
+                {
+                    //break;
+                    continue;
+                }
+
                 result.High = Math.Max(grades[index], result.High);
                 result.Low = Math.Min(grades[index], result.Low);
                 result.Average += grades[index];
