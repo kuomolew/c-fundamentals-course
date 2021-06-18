0d7b5f409c8cbb0d48e987b9093a61909fc3e2aa
Vladimir Mironov <kuomolew@gmail.com>
2021-06-18 22:41:03 +0300

    input from console

--- gradebook/src/Gradebook/Program.cs
+++ gradebook/src/Gradebook/Program.cs
@@ -8,9 +8,20 @@ namespace Gradebook
         static void Main(string[] args)
         {
             var book = new Book("Vova");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+
+            while (true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+                if (input == "q")
+                {
+                    break;
+
+                }
+                var grade = double.Parse(input);
+                book.AddGrade(grade);
+            };
+
             var stats = book.GetStatistics();
 
             System.Console.WriteLine($"The average grade is {stats.Average:N1} \nThe highest is {stats.High} \nThe lowest is {stats.Low} \nThe letter grade is {stats.Letter}");
